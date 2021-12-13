07560e272585a2166f315ca818b8a2be4ef675b9
kedirgamada <kedirgamada1@gmail.com>
2021-12-12 21:49:13 -0500

    players win or loose is modified.

--- board.py
+++ board.py
@@ -2,3 +2,11 @@ my_list = [['_', '_', '_'],
            ['_', '_', '_'],
            ['_', '_', '_']]
 print(my_list)
+
+player_1 = ''
+player_2 = ''
+winner = input("enter the winner: ")
+if winner == "player 1":
+    print("player 1 is a winner!")
+else:
+    print("player 2 is winner!")
